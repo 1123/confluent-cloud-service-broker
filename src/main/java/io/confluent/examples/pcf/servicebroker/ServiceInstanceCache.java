45b2a88659b91ac57fe6d38f7ad7f6d062953cf3
Benedikt Linse <benedikt@confluent.io>
2019-10-05 15:41:11 +0200

    #2 ensuring service isntances store topic is log compacted

--- src/main/java/io/confluent/examples/pcf/servicebroker/ServiceInstanceCache.java
+++ src/main/java/io/confluent/examples/pcf/servicebroker/ServiceInstanceCache.java
@@ -3,14 +3,11 @@ package io.confluent.examples.pcf.servicebroker;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.errors.TopicExistsException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.kafka.common.config.ConfigResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.task.TaskExecutor;
@@ -27,46 +24,68 @@ public class ServiceInstanceCache implements CommandLineRunner {
 
     @Value("${service.instance.store.topic}")
     private String serviceInstanceStoreTopic;
-
-    @Autowired
-    private KafkaConsumer<String,String> kafkaConsumer;
-
-    @Autowired
-    private AdminClient adminClient;
-
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Getter
     private Map<UUID, TopicServiceInstance> instances = new HashMap<>();
 
-    @Autowired
     private TaskExecutor taskExecutor;
+    private AdminClient adminClient;
+    private KafkaConsumer<String,String> kafkaConsumer;
 
-    private void createTopic() {
+    public ServiceInstanceCache(
+            KafkaConsumer<String, String> kafkaConsumer,
+            TaskExecutor taskExecutor,
+            AdminClient adminClient
+    ) {
+        this.kafkaConsumer = kafkaConsumer;
+        this.taskExecutor = taskExecutor;
+        this.adminClient = adminClient;
+    }
+
+    private void createServiceInstancesTopic() throws InterruptedException {
+        log.info("Creating service instances topic");
         CreateTopicsResult createTopicsResult = adminClient.createTopics(
                 Collections.singleton(new NewTopic(serviceInstanceStoreTopic, 1, (short) 1))
         );
         try {
             createTopicsResult.all().get();
-        } catch (ExecutionException | InterruptedException e) {
+        } catch (ExecutionException e) {
             log.info("Topic may already exist. ");
             log.info("e");
         }
     }
 
+    private void ensureServiceInstancesTopicIsLogCompacted() throws ExecutionException, InterruptedException {
+        log.info("Setting service instances topic to cleanup.policy log compacted ");
+        Map<ConfigResource, Collection<AlterConfigOp>> configs = new HashMap<>();
+        configs.put(
+                new ConfigResource(ConfigResource.Type.TOPIC, serviceInstanceStoreTopic),
+                Collections.singleton(
+                        new AlterConfigOp(
+                                new ConfigEntry("cleanup.policy", "compact"),
+                                AlterConfigOp.OpType.SET
+                        )
+                )
+        );
+        AlterConfigsResult alterConfigsResult = adminClient.incrementalAlterConfigs(configs);
+        log.info("AlterConfigsResult: " + alterConfigsResult.all().get());
+    }
+
     @Override
-    public void run(String... args) {
-        createTopic();
+    public void run(String... args) throws ExecutionException, InterruptedException {
+        createServiceInstancesTopic();
+        ensureServiceInstancesTopicIsLogCompacted();
         kafkaConsumer.subscribe(Collections.singletonList(serviceInstanceStoreTopic));
         taskExecutor.execute(() -> {
+            //noinspection InfiniteLoopStatement
             while (true) {
                 log.info("Caching service instances.");
-                    ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(Long.MAX_VALUE));
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(Long.MAX_VALUE));
                 log.info("Handling {} records", records.count());
                 records.forEach(this::handleRecord);
             }
         });
-
     }
 
     private void handleRecord(ConsumerRecord<String, String> record) {
